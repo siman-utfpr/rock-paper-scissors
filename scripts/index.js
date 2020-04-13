d5416f3067dbb5a66893e7a2f0a24a16cf448e04
Luis Eduardo Siman <siman@alunos.utfpr.edu.br>
2020-04-13 18:10:19 -0300

    Create the playRound and evaluateBattle functions
    The function playRound trims the user input and makes it lower case,
    then verifies if it is valid. If it is, the function evaluateBattle is
    called.

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,31 @@ function computerPlay() {
   let options = ["rock", "paper", "scissor"];
   return options[Math.floor(Math.random() * options.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+  let options = ["rock", "paper", "scissor"];
+  playerSelection = playerSelection.toLowerCase().trim();
+
+  if (!options.some((item) => item == playerSelection)) {
+    return "Please enter a valid option (rock, paper, scissor)";
+  }
+  return evaluateBattle(playerSelection, computerSelection);
+}
+
+function evaluateBattle(playerSelection, computerSelection) {
+  let matchTemplate = `${playerSelection} vs ${computerSelection}!`;
+  let winTemplate = `${matchTemplate} You WIN!`;
+  let loseTemplase = `${matchTemplate} You LOSE!`;
+  let drawTemplate = `${matchTemplate} DRAW!`;
+
+  if (playerSelection == computerSelection) return drawTemplate;
+  if (
+    (playerSelection == "rock" && computerSelection == "scissor") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissor" && computerSelection == "paper")
+  ) {
+    return winTemplate;
+  } else {
+    return loseTemplase;
+  }
+}
